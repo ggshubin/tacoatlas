a78942cad79ae9e5e1bd32d1fcccc9ff68bdea64
TacoAtlas Developer <developer@tacooatlas.local>
2026-06-09 08:34:52 -0700

    test: add expo-updates mock for jest

---
+++ src/__mocks__/expo-updates.ts
@@ -0,0 +1,5 @@
+export const isEmbeddedLaunch = true
+export const updateId: string | null = null
+export const runtimeVersion: string | null = null
+export const channel: string | null = null
+export const createdAt: Date | null = null
